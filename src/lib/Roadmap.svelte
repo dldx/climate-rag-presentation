b535e31007a32685110ac78874f926ee645b7683
Durand D'souza <durand1@gmail.com>
2024-11-08 04:47:55 +0000

    Adding comparisons between climate-rag and other options

--- src/lib/Roadmap.svelte
+++ src/lib/Roadmap.svelte
@@ -6,18 +6,18 @@
 
 <div
 	style="--road-opacity:{stepNo >= 0 ? '1' : '0'};
-	--reviewer-agent-opacity:{stepNo >= 1 ? '1' : '0'};
-	--multi-hop-agent-opacity:{stepNo >= 2 ? '1' : '0'};
-	--conversations-opacity:{stepNo >= 3 ? '1' : '0'};
-	--image-retrieval-opacity:{stepNo >= 4 ? '1' : '0'};
-	--sql-agent-opacity:{stepNo >= 5 ? '1' : '0'};
-	--graph-agent-opacity:{stepNo >= 6 ? '1' : '0'};
-	--dedicated-ui-opacity:{stepNo >= 7 ? '1' : '0'};
+	--reviewer-agent-opacity:{stepNo >= 0 ? '1' : '0'};
+	--multi-hop-agent-opacity:{stepNo >= 1 ? '1' : '0'};
+	--conversations-opacity:{stepNo >= 2 ? '1' : '0'};
+	--image-retrieval-opacity:{stepNo >= 3 ? '1' : '0'};
+	--sql-agent-opacity:{stepNo >= 4 ? '1' : '0'};
+	--graph-agent-opacity:{stepNo >= 5 ? '1' : '0'};
+	--dedicated-ui-opacity:{stepNo >= 6 ? '1' : '0'};
 	"
 >
 	{@html roadmap}
 </div>
-{#each { length: 7 } as _, i}
+{#each { length: 6 } as _, i}
 	<Transition
 		do={() => {
 			stepNo = i + 1
